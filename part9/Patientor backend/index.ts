e89e966b4caba4c44aa81747847447568a16cf79
Talar25 <jjasiota@gmail.com>
2023-11-15 14:44:55 +0100

    Add exercises 9.8-9.9

---
+++ part9/Patientor backend/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+const app = express();
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
+app.use(express.json());
+
+const PORT = 3001;
+
+app.get('/api/ping', (_req, res) => {
+  console.log('someone pinged here');
+  res.send('pong');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
